e2963ad05cb49cb18a7018a31c1803f7b5170bc2
Noor Muhammad <connectwithnoor1@gmail.com>
2023-02-25 02:12:45 +0500

    navbar component

--- app/{components}/navbar/navbar.tsx
+++ app/{components}/navbar/navbar.tsx
@@ -1,7 +1,29 @@
+import Image from 'next/image';
+
 import './navbar.scss';
+import NavbarResponsive from './navbarResponsive';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className='app__navbar'>
+      {/* logo */}
+      <div className='app__navbar-logo'>
+        <div className='app__navbar-logo-container'>
+          <Image src='/logo.png' alt='logo' fill />
+        </div>
+      </div>
+      {/* nav */}
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item, index) => (
+          <li key={item + index} className='app__flex p-text'>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+      <NavbarResponsive />
+    </nav>
+  );
 }
 
 export default Navbar;
---
+++ app/{components}/navbar/navbarResponsive.tsx
@@ -0,0 +1,58 @@
+'use client';
+import { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion, AnimatePresence, Variants } from 'framer-motion';
+
+import './navbar.scss';
+
+const motionVariance: Variants = {
+  hidden: {
+    opacity: 0,
+    x: 300,
+  },
+  show: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.85, ease: 'easeOut' },
+  },
+  exit: {
+    x: 300,
+    opacity: 0,
+    transition: { duration: 0.85, ease: 'easeOut' },
+  },
+};
+
+function NavbarResponsive() {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className='app__navbar-menu'>
+      <HiMenuAlt4 onClick={() => setToggle(true)} />
+      <AnimatePresence>
+        {toggle && (
+          <motion.div
+            variants={motionVariance}
+            initial='hidden'
+            whileInView='show'
+            exit='exit'
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map(
+                (item, index) => (
+                  <li key={item + index}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default NavbarResponsive;
